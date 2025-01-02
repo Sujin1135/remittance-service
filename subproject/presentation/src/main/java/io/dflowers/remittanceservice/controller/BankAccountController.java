1e32888537767e9be60d9793c6e93a612417d889
mango <mchead1@naver.com>
2025-01-02 21:47:29 +0900

    refactor: applied decorator as @RequiredArgsConstructor

--- subproject/presentation/src/main/java/io/dflowers/remittanceservice/controller/BankAccountController.java
+++ subproject/presentation/src/main/java/io/dflowers/remittanceservice/controller/BankAccountController.java
@@ -3,32 +3,19 @@ package io.dflowers.remittanceservice.controller;
 import io.dflowers.remittanceservice.domain.BankAccount;
 import io.dflowers.remittanceservice.dto.CreateAccountRequest;
 import io.dflowers.remittanceservice.service.bank.CreateBankAccount;
-import io.dflowers.remittanceservice.service.bank.FindBankById;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class BankAccountController {
-    private final FindBankById findBankById;
+
     private final CreateBankAccount createBankAccount;
-
-    public BankAccountController(
-        FindBankById findBankById,
-        CreateBankAccount createBankAccount
-    ) {
-        this.findBankById = findBankById;
-        this.createBankAccount = createBankAccount;
-    }
-
-    @GetMapping("/accounts")
-    BankAccount getAccounts() {
-        return findBankById.apply(5L);
-    }
 
     @PostMapping("/accounts")
     @ResponseStatus(HttpStatus.CREATED)
